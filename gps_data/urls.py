7eb73b76add3b217110d69cc499de47f8d94e473
Horraira <abuhorraira1432@gmail.com>
2024-11-16 21:04:57 +0600

    installed stripe

--- gps_data/urls.py
+++ gps_data/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('', gps_data, name='gps_data')
-    path('', views.GPSDataListCreate.as_view(), name='gps_data')
+    path('', views.GPSDataListCreate.as_view(), name='gps_data'),
+    path('location/', views.show_last_location, name='last_location'),
 ]
--- gps_data/views.py
+++ gps_data/views.py
@@ -1,9 +1,10 @@
-from rest_framework import status, generics
-from rest_framework.response import Response
+from django.shortcuts import render
+from rest_framework import generics, status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import GPSData
 from .serializers import GPSDataSerializer
-
 
 
 class GPSDataListCreate(generics.ListCreateAPIView):
@@ -12,13 +13,13 @@ class GPSDataListCreate(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return GPSData.objects.all().order_by('-id')[:20]
+    
 
-
-# @api_view(['POST'])
-# def gps_data(request):
-#     if request.method == 'POST':
-#         serializer = GPSDataSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def show_last_location(request):
+    # Get the latest GPSData entry
+    last_location = GPSData.objects.latest('timestamp')
+    context = {
+        'latitude': last_location.latitude,
+        'longitude': last_location.longitude,
+    }
+    return render(request, 'last_location.html', context)
